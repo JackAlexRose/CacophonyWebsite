23f9fd987a9e2a4735f94873e34ff0cecfacffd0
Jack Rose <jackalexanderrose@hotmail.com>
2020-02-22 12:08:04 +0000

    Updated stylesheet

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,18 +1,19 @@
 let randOffset = 5;
 var canvas;
 var mess;
-let c = color('magenta');
+let c = color('#FE4366');
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0,0);
   canvas.style('z-index', '-1');
-  background(255);
+  background(230, 210, 230);
 }
 
 function draw() {
-  background(0);
-  stroke(240);
+  background(230, 210, 230);
+  strokeWeight(2);
+  stroke('#96404F');
   for (let x = 100; x <= width - 100; x += 10) {
     for (let y = 100; y <= height - 100; y += 10) {
       if (pow((x - (width / 1.5)), 2) + pow((y - (height / 2)), 2) < 50000) {
