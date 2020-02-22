c79bb11df83d0d803b68fb01963376721cc7c84a
JackAlexRose <jackalexanderrose@hotmail.com>
2020-02-22 10:31:52 +0000

    Initial Commit

---
+++ scripts/sketch.js
@@ -0,0 +1,27 @@
+let randOffset = 5;
+var canvas;
+var mess;
+let c = color('magenta');
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0,0);
+  canvas.style('z-index', '-1');
+  background(255);
+}
+
+function draw() {
+  background(0);
+  stroke(240);
+  for (let x = 100; x <= width - 100; x += 10) {
+    for (let y = 100; y <= height - 100; y += 10) {
+      if (pow((x - (width / 1.5)), 2) + pow((y - (height / 2)), 2) < 50000) {
+        line(x - random(mouseY/8), y - random(mouseY/8), x + 5 + random(mouseY/8), y + random(mouseY/8));
+      }
+    }
+  }
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
